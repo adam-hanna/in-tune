8ea1a5b6fdb1e41e9cc1827577d746dabe0aef58
Adam Hanna <ahanna@alumni.mines.edu>
2020-12-09 15:41:09 -0800

    adds ui

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate vst;
+extern crate vst_gui;
+extern crate serde_json;
 
+use std::include_str;
 use vst::api;
 use vst::buffer::{AudioBuffer, SendEventBuffer};
 use vst::event::{Event, MidiEvent};
@@ -8,33 +11,62 @@ use vst::plugin::{CanDo, HostCallback, Info, Plugin};
 
 plugin_main!(MyPlugin); // Important!
 
-#[repr(u8)]
-#[derive(Copy, Clone)]
-pub enum Step {
-    SemiTone = 1,
-    Tone = 2,
+const HTML: &'static str = include_str!("../ui/build/index.html");
+
+struct KeyMapper {
+    key: Option<u8>,
+    scale: Option<Vec<u8>>,
 }
 
-pub static MAJOR_SCALE: &'static [Step] = &[Step::Tone, Step::Tone, Step::SemiTone, Step::Tone, Step::Tone, Step::Tone, Step::SemiTone];
-
-fn transpose_pressed_key(key: u8, pressed_key: u8) -> Option<u8> {
-    let remainder = pressed_key%12 - key%12;
-    if remainder as u8 > MAJOR_SCALE.len() as u8 {
-        return None;
-    }
-
-    let mut offset: u8 = 0;
-    for (i, item) in MAJOR_SCALE.iter().enumerate() {
-        if i as u8 >= remainder as u8 {
-            break;
+impl KeyMapper {
+    fn transpose_pressed_key(&mut self, pressed_key: u8) -> Option<u8> {
+        if !self.key.is_some() || !self.scale.is_some() {
+            return Some(pressed_key);
         }
 
-        offset += *item as u8;
-    }
-    
-    let base = pressed_key as i8/12 - key as i8/12;
+        let remainder = pressed_key%12 - self.key.unwrap()%12;
+        if remainder as u8 > self.scale.as_ref().unwrap().len() as u8 {
+            return None;
+        }
 
-    Some((key as i8 + base*12 + offset as i8) as u8) 
+        let offset: u8 = self.scale.as_ref().unwrap()[remainder as usize];
+        let base = pressed_key as i8/12 - self.key.unwrap() as i8/12;
+
+        Some((self.key.unwrap() as i8 + base*12 + offset as i8) as u8) 
+    }
+}
+
+fn create_javascript_callback(
+    key_mapper: Arc<Mutex<KeyMapper>>) -> vst_gui::JavascriptCallback
+{
+    Box::new(move |message: String| {
+        let mut tokens = message.split_whitespace();
+
+        let command = tokens.next().unwrap_or("");
+        match command {
+            "stop" => {
+                let mut locked_key_mapper = key_mapper.lock().unwrap();
+                locked_key_mapper.key = None;
+                locked_key_mapper.scale = None;
+
+                return String::new()
+            },
+            "set" => {
+                let key = tokens.next().unwrap_or("").parse::<u8>();
+                let scaleStr = tokens.next().unwrap_or("").parse::<String>();
+                let scale: Vec<u8> = serde_json::from_str(scaleStr).unwrap();
+
+                let mut locked_key_mapper = key_mapper.lock().unwrap();
+
+                locked_key_mapper.key = Some(key);
+                locked_key_mapper.scale = Some(scale);
+                return String::new()
+            },
+            _ => {}
+        }
+
+        String::new()
+    })
 }
 
 #[derive(Default)]
@@ -42,6 +74,7 @@ struct MyPlugin {
     host: HostCallback,
     events: Vec<MidiEvent>,
     send_buffer: SendEventBuffer,
+    key_mapper: Arc<Mutex<KeyMapper>>
 }
 
 impl MyPlugin {
@@ -64,6 +97,8 @@ impl Plugin for MyPlugin {
             unique_id: 7357001, // Used by hosts to differentiate between plugins.
             inputs: 2,
             outputs: 2,
+            midi_inputs: 1,
+            midi_outputs: 1,
             parameters: 0,
             initial_delay: 0,
             ..Info::default()
@@ -75,7 +110,8 @@ impl Plugin for MyPlugin {
             #[allow(clippy::single_match)]
             match e {
                 Event::Midi(mut e) => {
-                    let new_key: Option<u8> = transpose_pressed_key(24, e.data[1]);
+                    let locked_key_mapper = self.key_mapper.lock().unwrap();
+                    let new_key: Option<u8> = locked_key_mapper.transpose_pressed_key(e.data[1]);
                     match new_key {
                         Some(inner) => {
                             e.data[1] = inner;
@@ -105,6 +141,14 @@ impl Plugin for MyPlugin {
             }
         }
         self.send_midi();
+    }
+    
+    fn get_editor(&mut self) -> Option<Box<dyn Editor>> {
+        let gui = vst_gui::new_plugin_gui(
+            String::from(HTML),
+            create_javascript_callback(self.key_mapper.clone()),
+            Some((1500, 800)));
+        Some(Box::new(gui))
     }
 
     fn can_do(&self, can_do: CanDo) -> vst::api::Supported {
